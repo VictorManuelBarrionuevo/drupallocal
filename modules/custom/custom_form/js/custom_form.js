485c6f47cb8df65ace7e7d9437dd298086dd6a31
jnahuel46 <jnahuel46@gmail.com>
2020-11-14 19:36:05 -0300

    asas

--- modules/custom/custom_form/js/custom_form.js
+++ modules/custom/custom_form/js/custom_form.js
@@ -1,19 +1,20 @@
 (function ($) {
     $(document).ready(function () {
 
-        window.array_paises = [];
+        window.array_paises = [];//array global de paises
         $("#pais_prov li").each(function () {
-            var pais_id = $(this).attr("pais-id");
-            var pais_name = $(this).attr("pais-name");
-            array_paises[pais_id] = pais_name;
+            var pais_id = $(this).attr("pais-id");//se le pasa el atributo attr al this(#pais_prov list) pais_id
+            var pais_name = $(this).attr("pais-name");//lo mismo que arriba pero le pasamos pais-name
+            array_paises[pais_id] = pais_name;//recorro el array paises con el key pais:id y el value pais name
         });
 
         window.options_for_paises = '';
-        array_paises.forEach(function(pais, key) {
-            options_for_paises += '<option value="' + key + '">' + pais + '</option>';
+        array_paises.forEach(function(pais, key) {// una vez creado el array paises lo meto en un for each con key value
+            options_for_paises += '<option value="' + key + '">' + pais + '</option>';//creo otro arrray global optionfor paies y concateno el key value(pais)
         });
 
-        $('#pais').html(options_for_paises);
+        $('#pais').html(options_for_paises);//traigo el id y con html le paso el resultado del array
+        //tendria que traer el resultado de optionsfor paises y a eso relacionarle la provincia
 
         $("#especificar").hide();
         $('input[type=radio][name=gender]').change(function () {
