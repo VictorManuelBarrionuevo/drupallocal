b94f0a8933731f9859a5b1d62d44b43536095000
jnahuel46 <jnahuel46@gmail.com>
2020-11-14 18:06:01 -0300

    otros

--- modules/custom/custom_form/js/custom_form.js
+++ modules/custom/custom_form/js/custom_form.js
@@ -1,6 +1,17 @@
 (function ($) {
     $(document).ready(function () {
-        console.log('holanda');
+        $("#especificar").hide();
+        $('input[type=radio][name=gender]').change(function () {
+            if (this.value == 'male') {
+                $("#especificar").hide();
+            }
+            else if (this.value == 'female') {
+                $("#especificar").hide();
+            }
+            else if (this.value == 'other') {
+                $("#especificar").show();
+            }
+        });
 
 
     });
