50473d024e919c1c48ebf30cf221f1b9f6174f70
Vito <victor.barrionuevo@darwoft.com>
2020-11-14 19:21:40 -0300

    actualizacion de paises

--- modules/custom/custom_form/js/custom_form.js
+++ modules/custom/custom_form/js/custom_form.js
@@ -1,5 +1,20 @@
 (function ($) {
     $(document).ready(function () {
+
+        window.array_paises = [];
+        $("#pais_prov li").each(function () {
+            var pais_id = $(this).attr("pais-id");
+            var pais_name = $(this).attr("pais-name");
+            array_paises[pais_id] = pais_name;
+        });
+
+        window.options_for_paises = '';
+        array_paises.forEach(function(pais, key) {
+            options_for_paises += '<option value="' + key + '">' + pais + '</option>';
+        });
+
+        $('#pais').html(options_for_paises);
+
         $("#especificar").hide();
         $('input[type=radio][name=gender]').change(function () {
             if (this.value == 'male') {
--- modules/custom/custom_form/src/Controller/CustomFormController.php
+++ modules/custom/custom_form/src/Controller/CustomFormController.php
@@ -20,10 +20,22 @@ class CustomFormController
      */
     public function add_new()
     {
-        return [
-            '#theme' => 'custom_form'
+        $database = \Drupal::database();
 
+        // BUSCAR LISTADO DE PROVINCIAS Y PAISES
+        $query_pais_prov = "SELECT pa.id as pais_id, pa.name as pais_name, pr.id as provincia_id, pr.name as provincia_name 
+                            FROM provincias pr
+                            inner join paises pa 
+                            on pa.id = pr.id_pais;";
+        $pais_prov = $database->query($query_pais_prov)->fetchAll();
+        $variables = [];
+        $variables['pais_prov'] = $pais_prov;
+
+        return [
+            '#theme' => 'custom_form',
+            '#variables' => $variables
         ];
+        
     }
     public function save_new()
     {
