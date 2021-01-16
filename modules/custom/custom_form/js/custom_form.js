0afe91a99498dd487a79c4fc588eff69be9bc0e6
Jere <jnahuel46@gmail.com>
2021-01-16 19:47:53 -0300

    defiiiii

--- modules/custom/custom_form/js/custom_form.js
+++ modules/custom/custom_form/js/custom_form.js
@@ -1,108 +1,80 @@
 (function ($) {
   $(document).ready(function () {
-   $(".ocultar").hide();
+    //$("input").prop('required',true);
+    $(".ocultar").hide();
     window.array_paises = [];
     $("#pais_prov li").each(function () {
-      var pais_selected = $(this).attr("elemento-seleccionado");
       var pais_id = $(this).attr("pais-id");
       var pais_name = $(this).attr("pais-name");
       array_paises[pais_id] = pais_name;
-      if (pais_selected == 1) {
-        window.options_for_paises +=
-          '<option selected disabled hidden [value="' +
-          pais_selected +
-          '"]>' +
-          pais_name +
-          "</option>";
-      } else {
-        window.options_for_paises =
-          "<option selected disabled hidden value=''>--Seleccionar pais--</option>";
-      }
     });
 
+    window.options_for_paises =
+      "<option selected disabled hidden style='display: none' value=''>--Seleccionar pais--</option>";
     array_paises.forEach(function (pais, key) {
       options_for_paises += '<option value="' + key + '">' + pais + "</option>";
     });
-
     $("#pais").html(options_for_paises);
 
+    $("#pais_prov li").each(function () {
+      var elemento_selected = $(this).attr("elemento-seleccionado");
+      var pais_id = $(this).attr("pais-id")
+      if (elemento_selected == 1) {
+        $("#pais").val(pais_id).trigger('change');//me dice que cuando cambien pais, se dispara a la siguiete funcion
+        window.id_pais_seleccionado = pais_id;
+        window.array_provincias = [];
+        $("#pais_prov li").each(function () {
+          var pais_id = $(this).attr("pais-id");
+          if (pais_id == id_pais_seleccionado) {
+            var provincia_id = $(this).attr("provincia-id");
+            var provincia_name = $(this).attr("provincia-name");
+            array_provincias[provincia_id] = provincia_name;
+          }
+        });
+        window.options_for_provincias = "";
+        array_provincias.forEach(function (provincia, key) {
+          options_for_provincias +=
+            '<option value="' + key + '">' + provincia + "</option>";
+        });
+        $("#provincia").html(options_for_provincias);
+        $("#provincia").val($(this).attr("provincia-id"))
+        $(".ocultar").show();
+
+
+      }
+    
+
+
+
+    });
 
 
 
 
-      window.array_provincias_selected = [];
-      $("#pais_prov li").each(function () {
-      var pais_selected = $(this).attr("elemento-seleccionado");
-      var provincia_id = $(this).attr("provincia-id");
-      var provincia_name = $(this).attr("provincia-name");
-      array_provincias_selected[provincia_id] = provincia_name;
-      if (pais_selected == 1) {
-        window.options_for_provincia_selected +=
-          '<option selected disabled hidden [value="' +
-          pais_selected +
-          '"]>' +
-          provincia_name +
-          "</option>";
-       }
-       else{
-        window.options_for_provincia_selected +="";
-       }
-       });
-    
-       options_for_provincia_selected +="";
-   
 
-       $("#provincia").html(options_for_provincia_selected);
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
     $("#pais").on("change", function () {
       window.id_pais_seleccionado = $(this).val();
       window.array_provincias = [];
       $("#pais_prov li").each(function () {
-        var pais_selected = $(this).attr("elemento-seleccionado");
         var pais_id = $(this).attr("pais-id");
         if (pais_id == id_pais_seleccionado) {
           var provincia_id = $(this).attr("provincia-id");
           var provincia_name = $(this).attr("provincia-name");
           array_provincias[provincia_id] = provincia_name;
         }
-        /*if(pais_selected == 1 ){
-          window.options_for_provincias +='<option selected disabled hidden [value="' + pais_selected + '"]>' + provincia_name + "</option>";
-        }
-        else {
-          window.options_for_provincias = "";
-          
-        }*/
-
-
-
       });
       window.options_for_provincias = "";
-
-      
-
       array_provincias.forEach(function (provincia, key) {
         options_for_provincias +=
           '<option value="' + key + '">' + provincia + "</option>";
       });
       $("#provincia").html(options_for_provincias);
-
       $(".ocultar").show();
     });
 
     $("#especificar").hide();
-
     $("input[type=radio][name=gender]").change(function () {
       if (this.value == "male") {
         $("#especificar").hide();
@@ -115,30 +87,4 @@
   });
 })(jQuery);
 
-/*$("#pais_prov li").each(function () {
-        var pais_selected = $(this).attr("elemento-seleccionado");
-        var pais_id = $(this).attr("pais-id");
-        if (pais_id == id_pais_seleccionado) {
-          var provincia_id = $(this).attr("provincia-id");
-          var provincia_name = $(this).attr("provincia-name");
-          array_provincias[provincia_id] = provincia_name;
-        }
-        });
 
-
-        if(pais_selected == 1 ){
-          window.options_for_provincias +='<option selected disabled hidden [value="' + pais_selected + '"]>' + provincia_name + "</option>";
-         }
-
-
-
-          if (pais_selected == 1) {
-          window.options_for_provincias +=
-            '<option selected disabled hidden [value="' +
-            pais_selected +
-            '"]>' +
-            provincia_name +
-            "</option>";
-        }
-
-        */
