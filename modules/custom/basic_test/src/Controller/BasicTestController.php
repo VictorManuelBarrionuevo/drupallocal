670b08a06989b4292f2bd35bf71cb14dab8f3a05
Vito <victor.barrionuevo@darwoft.com>
2021-01-16 19:46:51 -0300

    Clase de Insert

--- modules/custom/basic_test/src/Controller/BasicTestController.php
+++ modules/custom/basic_test/src/Controller/BasicTestController.php
@@ -24,4 +24,37 @@ class BasicTestController
             '#theme' => 'basic_test_theme_test'
         ];
     }
+
+    public function insercion(){
+
+        $database = \Drupal::database();
+        $nombre = $_GET['fname'];
+        $apellido = $_GET['lname'];
+        $query = "INSERT INTO inserts (`nombre`, `apellido`) VALUES ('$nombre', '$apellido');";
+        $database->query($query);
+
+        
+        $array_vars = [];
+        $array_vars['nombre'] = $nombre;
+        $array_vars['apellido'] = $apellido;
+        return [
+            '#theme' => 'success_insert_twig',
+            '#vars' => $array_vars
+        ];
+    
+    }
+
+    public function get_inserts(){
+        $database = \Drupal::database();
+        $query = "SELECT * FROM inserts;";
+        $listado = $database->query($query)->fetchAll();
+
+        return[
+            '#theme' => 'list_page',
+            '#vars' => $listado
+        ];
+    }
+
+
+
 }
